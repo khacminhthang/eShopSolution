947a42c7a9bd7743df838ffedd52673655d2731c
Minh Thang Le Khac <thanglkm@vnpt.vn>
2022-03-09 00:03:54 +0700

    @

--- eShopSolution.Application/System/Users/UserService.cs
+++ eShopSolution.Application/System/Users/UserService.cs
@@ -110,7 +110,9 @@ namespace eShopSolution.Application.System.Users
             //4. Select and projection
             var pagedResult = new PagedResult<UserViewModel>()
             {
-                TotalRecord = totalRow,
+                TotalRecords = totalRow,
+                PageIndex = request.PageIndex,
+                PageSize = request.PageSize,
                 Items = data
             };
             return new ApiSuccessResult<PagedResult<UserViewModel>>(pagedResult);
